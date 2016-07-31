ad273101f84e35510f711802d4b4cf584b5491f6
tbenyon <tom.benyon@gmail.com>
2016-07-31 10:10:15 +0000

    Update client python file for Pi.

--- doorBellClient.py
+++ doorBellClient.py
@@ -4,40 +4,43 @@ from picamera import PiCamera
 from time import sleep
 import os
 import RPi.GPIO as GPIO
+from time import gmtime, strftime
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(17, GPIO.IN)#, pull_up_down=GPIO.PUD_UP)
 camera = PiCamera()
-	
+    
 def takeNewImage():
-	try:
-		os.remove('/home/pi/image.jpg')
-	except:
-		print("No image to remove")
+    try:
+        os.remove('/home/pi/image.jpg')
+    except:
+        print("No image to remove")
 
-	camera.start_preview()
-	sleep(1)
+    camera.start_preview()
+    sleep(1)
 
-	camera.capture('/home/pi/image.jpg')
-	camera.stop_preview()
-	
+    camera.capture('/home/pi/image.jpg')
+    camera.stop_preview()
+    
 def uploadImage():
-	try:
-		with open('/home/pi/image.jpg', "rb") as imageFile:
-			base64Image = base64.b64encode(imageFile.read())
-		response = requests.post(os.environ['API_URL'], data={"image": base64Image})
+    try:
+        with open('/home/pi/image.jpg', "rb") as imageFile:
+            base64Image = base64.b64encode(imageFile.read())
+        response = requests.post('https://suphomie.herokuapp.com/addImageData', data={"image": base64Image})
 
-		print ("The response...")
-		print (response)
-		
-	except:
-		print("Failed to upload image.")
+        print ("The response...")
+        print (response)
+        
+    except:
+        print("Failed to upload image.")
 
 print('Starting door camera system.')
 while True:
-    input_state = GPIO.input(18)
-    if input_state == False:
-        print('Matt Triggered')
+    sleep(0.1)
+    input_state = GPIO.input(17)
+    if input_state == 1:
+        print('Matt Triggered: ' + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+        
         takeNewImage()
         uploadImage()
-        sleep(10)
+        sleep(60*5)
